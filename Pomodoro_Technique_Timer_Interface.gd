28f34df4d017472adff3552baf3deeb1badafddb
frumsrekovni <joshua.backstrom@gmail.com>
2022-01-12 05:43:10 +0100

    Displays timer on screen

--- Pomodoro_Technique_Timer_Interface.gd
+++ Pomodoro_Technique_Timer_Interface.gd
@@ -4,18 +4,23 @@ extends Control
 # Declare member variables here. Examples:
 # var a: int = 2
 # var b: String = "text"
-
+onready var work_time = get_node("Work_timer")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	work_time.set_wait_time(5)
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta: float) -> void:
-#	pass
+func _process(delta: float) -> void:
+	$Time_remaining.text = str(work_time.time_left)
 
 
 func _on_Button_pressed() -> void:
+	work_time.start()
 	print("Hello World!")
-	pass # Replace with function body.
+
+func _on_Work_timer_timeout() -> void:
+	work_time.stop()
+	
