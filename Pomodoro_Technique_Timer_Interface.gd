1d4185b00c876cebdb5f405c1c3365d9e9115f66
frumsrekovni <joshua.backstrom@gmail.com>
2022-01-12 23:11:58 +0100

    added an ugly title and input bar

--- Pomodoro_Technique_Timer_Interface.gd
+++ Pomodoro_Technique_Timer_Interface.gd
@@ -9,13 +9,11 @@ onready var work_time = get_node("Work_timer")
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	work_time.set_wait_time(5)
-	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	$Time_remaining.text = str(work_time.time_left)
-
 
 func _on_Button_pressed() -> void:
 	work_time.start()
@@ -24,3 +22,9 @@ func _on_Button_pressed() -> void:
 func _on_Work_timer_timeout() -> void:
 	work_time.stop()
 	
+
+
+func _on_LineEdit_text_entered(new_text: String) -> void:
+	work_time.set_wait_time(int(new_text))
+	work_time.start()
+	$LineEdit.clear()
